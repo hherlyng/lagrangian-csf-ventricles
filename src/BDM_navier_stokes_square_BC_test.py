4e3408381d8de88ab6a00672936e7c5d832721a8
hherlyng <hherlyng@hotmail.com>
2025-04-16 11:21:04 +0200

    BC test Navier-Stokes square

--- src/BDM_navier_stokes_square_BC_test.py
+++ src/BDM_navier_stokes_square_BC_test.py
@@ -7,21 +7,15 @@ import dolfinx as dfx
 from sys    import argv
 from ufl    import div, dot, grad, inner, nabla_grad
 from scifem import assemble_scalar
-from utilities.fem     import calculate_mean, calculate_norm_L2, eps, stabilization
+from utilities.fem     import calculate_mean, calculate_norm_L2, eps, stabilization, create_normal_contribution_bc, tangent
 from utilities.mesh    import create_unit_square_mesh
 from dolfinx.fem.petsc import assemble_matrix_block, assemble_vector_block, create_matrix_block, create_vector_block
-
-# Parabolic velocity profile
-def u_parabolic(x):
-    return np.vstack((4*Re*(x[1]-x[1]**2),
-                      np.zeros_like(x[1])
-    ))
 
 # Simulation parameters
 comm = MPI.COMM_WORLD # MPI communicator
 N = int(argv[1]) # Mesh cells
 t = 0.0
-num_time_steps = 200
+num_time_steps = 100
 t_end = 1
 delta_t = t_end/num_time_steps # Timestep size
 mu_value  = 1e-2 # Dynamic viscosity
@@ -49,11 +43,9 @@ u, p = ufl.TrialFunctions(M)
 v, q = ufl.TestFunctions(M)
 
 u_ = dfx.fem.Function(V) # Velocity at previous timestep
-u_.interpolate(u_parabolic) # Interpolate initial condition
 
 dt = dfx.fem.Constant(mesh, dfx.default_real_type(delta_t)) # Timestep
-gamma = dfx.fem.Constant(mesh, dfx.default_real_type(1000.0)) # BDM penalty parameter
-beta = dfx.fem.Constant(mesh, dfx.default_scalar_type(1000.0)) # Nitsche penalty parameter
+gamma = dfx.fem.Constant(mesh, dfx.default_real_type(10.0)) # BDM penalty parameter
 mu = dfx.fem.Constant(mesh, dfx.default_real_type(mu_value)) # Dynamic viscosity
 rho = dfx.fem.Constant(mesh, dfx.default_scalar_type(rho_value)) # Fluid density
 
@@ -65,51 +57,69 @@ n = ufl.FacetNormal(mesh)
 dx = ufl.Measure('dx', mesh) # Cell integral
 ds = ufl.Measure('ds', mesh, subdomain_data=ft) # Facet integral
 
-# Navier-Stokes equations bilinear form in block form
+# Tangential traction BC
+tangential_traction = ufl.as_vector((1.0, 0.0))
+
+# Create stokes problem to solve for initial condition
+# Stokes equations bilinear form in block form
 a00 = (rho/dt * inner(u , v) * dx # Time derivative
-     + rho*inner(dot(u_, nabla_grad(u)), v) * dx # Convective term
      + 2*mu*inner(eps(u), eps(v))*dx # Viscous dissipation
      + stabilization(u, v, mu, gamma) # BDM stabilization
      - mu*inner(dot(grad(u).T, n), v) * (ds(LEFT) + ds(RIGHT)) # Parallel flow at outlet
-     + beta/h*inner(u, v)*(ds(TOP)+ds(BOT))
     )
 a01 = -inner(p, div(v))*dx
 a10 = -inner(q, div(u))*dx
 a11 = dfx.fem.Constant(mesh, dfx.default_scalar_type(0.0))*inner(p, q)*dx
 
 # Linear form
-L0 = rho/dt * inner(u_, v) * dx # Time derivative
-# L0 -= dot(dfx.fem.Constant(mesh, dfx.default_scalar_type(8.0))*n, v) * ds(LEFT) # Impose pressure
+L0  = rho/dt * inner(u_, v) * dx # Time derivative
+L0 += inner(tangential_traction, tangent(v, n)) * ds(TOP)
         
 L1 = inner(dfx.fem.Function(Q), q)*dx
 
-a = dfx.fem.form([[a00, a01],
-                  [a10, a11]])
+a_stokes = dfx.fem.form([[a00, a01],
+                         [a10, a11]])
 L = dfx.fem.form([L0, L1])
 
+# Navier-Stokes equations bilinear form in block form
+a00 += rho*inner(dot(u_, nabla_grad(u)), v) * dx # Convective term
+
+a = dfx.fem.form([[a00, a01],
+                  [a10, a11]])
+
 # Strong boundary conditions: Impermeability on top/bottom
-impermeability_dofs = dfx.fem.locate_dofs_topological(V, facet_dim, np.concatenate((ft.find(TOP), ft.find(BOT))))
+impermeability_dofs = dfx.fem.locate_dofs_topological(V, facet_dim, ft.find(TOP))
 bc_impermeability = dfx.fem.dirichletbc(dfx.fem.Function(V), impermeability_dofs)
 bcs = [bc_impermeability]               
 
-inflow = dfx.fem.Function(V)
-inflow.interpolate(u_parabolic)
-bc_inflow = dfx.fem.dirichletbc(inflow, dfx.fem.locate_dofs_topological(V, facet_dim, ft.find(LEFT)))
+influx = create_normal_contribution_bc(V, -1/4*n, ft.find(LEFT))
+bc_inflow = dfx.fem.dirichletbc(influx, dfx.fem.locate_dofs_topological(V, facet_dim, ft.find(LEFT)))
 bcs.append(bc_inflow)
 
-# Assemble the Navier-Stokes problem
+defo = dfx.fem.Function(V)
+defo.interpolate(lambda x: (x[0], x[0]))
+bc_defo = dfx.fem.dirichletbc(defo, dfx.fem.locate_dofs_topological(V, facet_dim, ft.find(BOT)))
+bcs.append(bc_defo)
+
+# Assemble the Stokes problem
+A_stokes = assemble_matrix_block(a_stokes,bcs=bcs)
+A_stokes.assemble()
+b = assemble_vector_block(L, a_stokes, bcs=bcs)
+
+# Create the Navier-Stokes problem
 A = create_matrix_block(a)
-b = create_vector_block(L)
 x = A.createVecRight() # Solution vector
 
 # Create and configure solver
 ksp = PETSc.KSP().create(comm)  # type: ignore
-ksp.setOperators(A)
+ksp.setOperators(A_stokes)
 ksp.setType("preonly")
 ksp.getPC().setType("lu")
 ksp.getPC().setFactorSolverType("mumps")
 opts = PETSc.Options()  # type: ignore
 opts["mat_mumps_icntl_14"] = 80  # Increase MUMPS working memory
+opts["mat_mumps_icntl_24"] = 1  # Option to support solving a singular matrix (pressure nullspace)
+opts["mat_mumps_icntl_25"] = 0  # Option to support solving a singular matrix (pressure nullspace)
 opts["ksp_error_if_not_converged"] = 1 # Throw an error if KSP solver does not converge
 ksp.setFromOptions()
 
@@ -117,19 +127,33 @@ ksp.setFromOptions()
 u_h = dfx.fem.Function(V)
 p_h = dfx.fem.Function(Q)
 
+# Create velocity function for visualization
 u_vis = dfx.fem.Function(W)
 u_vis.name = "u"
-u_vis.interpolate(u_)
 
-# Write initial condition to file
+# Create output files
 u_file = dfx.io.VTXWriter(comm, "u.bp", u_vis)
 p_file = dfx.io.VTXWriter(comm, "p.bp", p_h)
-u_file.write(t)
-p_file.write(t)
 
 # Create offsets for the blocked functions
 offset_u = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
 offset_p = offset_u + Q.dofmap.index_map.size_local*Q.dofmap.index_map_bs
+
+# Solve Stokes problem for the initial condition
+ksp.solve(b, x)
+u_.x.array[:offset_u] = x.array_r[:offset_u]
+u_.x.scatter_forward()
+u_vis.interpolate(u_) # Interpolate velocity into visualization function
+p_h.x.array[:(offset_p-offset_u)] = x.array_r[offset_u:offset_p]
+p_h.x.scatter_forward()
+p_h.x.array[:] -= calculate_mean(mesh, p_h, dX=dx)
+
+# Write initial condition to file
+u_file.write(t)
+p_file.write(t)
+
+# Set Navier-Stokes system matrix as KSP operator
+ksp.setOperators(A)
 
 # Time stepping loop
 for _ in range(num_time_steps):
@@ -163,20 +187,16 @@ for _ in range(num_time_steps):
 u_file.close()
 p_file.close()
 
-
 # Compute divergence L2 norm to check mass conservation
 e_div_u = calculate_norm_L2(comm, div(u_h), dX=dx)
 # assert np.isclose(e_div_u, 0.0, atol=float(1.0e5 * np.finfo(dfx.default_real_type).eps))
 
-# Calculate error in velocity approximation
-u_exact = dfx.fem.Function(V)
-u_exact.interpolate(u_parabolic)
-L2_error = assemble_scalar(
-                dfx.fem.form(
-                    inner(u_h-u_exact, u_h-u_exact) * dx
-                )
+flux = assemble_scalar(
+            dfx.fem.form(
+                dot(u_h, n) * ds(LEFT)
             )
-L2_error = np.sqrt(L2_error)
+)
+
 if comm.rank == 0:
     print(f"e_div_u = {e_div_u}")
-    print(f"e_u_L2 = {L2_error}")
+    print(f"influx = {flux}")
