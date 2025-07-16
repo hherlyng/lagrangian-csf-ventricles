f414ffe99ddba1b321fed8911ccc8a95db25eb08
hherlyng <hherlyng@hotmail.com>
2025-07-16 15:44:48 +0200

    Nitsche normal displacement; updated data

--- src/linear_elastodynamics.py
+++ src/linear_elastodynamics.py
@@ -5,7 +5,7 @@ import numpy   as np
 import dolfinx as dfx
 import adios4dolfinx as a4d
 
-from ufl       import inner, grad, sym
+from ufl       import inner, grad, sym, dot
 from mpi4py    import MPI
 from petsc4py  import PETSc
 from basix.ufl import element
@@ -15,7 +15,9 @@ from utilities.fem        import assemble_system
 from utilities.projection import projection_problem_CG_to_BDM
 from utilities.deformation_data import (DisplacementCorpusCallosumCephalocaudal, 
                                         DisplacementCaudateNucleusHeadLateral,
-                                        DisplacementThirdVentricleLateral)
+                                        DisplacementThirdVentricleLateral,
+                                        DisplacementLateralVentricleHorns,
+                                        DisplacementVentricleFloorCephalocaudal)
 
 """ Solve the time-dependent equations of linear elasticity with
     a finite element method based on continuous Lagrange elements.
@@ -24,10 +26,22 @@ from utilities.deformation_data import (DisplacementCorpusCallosumCephalocaudal,
 
 print = PETSc.Sys.Print # Only print from rank 0
 
-# Mesh tags
-CANAL_WALL = 13
+# Facet tags
+CANAL_WALL = 13 
+CANAL_OUT = 23
+THIRD_VENTRICLE_WALL = 14
+THIRD_VENTRICLE_FORAMINA = 46
+AQUEDUCT_WALL = 15
+AQUEDUCT_TOP = 45
+AQUEDUCT_BOT = 58
+FORAMINA_34_WALL = 16
+LATERAL_VENTRICLES_FORAMINA = 67
+LATERAL_VENTRICLES_WALL = 17
 FOURTH_VENTRICLE_WALL = 18
-CANAL_OUT  = 23
+FOURTH_VENTRICLE_OUT = 38
+CHOROID_PLEXUS_LATERAL = 101
+CHOROID_PLEXUS_THIRD = 103
+CHOROID_PLEXUS_FOURTH = 104
 LATERAL_APERTURES = 28
 CORPUS_CALLOSUM = 110
 THIRD_RIGHT = 111
@@ -36,6 +50,18 @@ LATERAL_RIGHT = 113
 LATERAL_LEFT = 114
 THIRD_ANTERIOR = 115
 THIRD_POSTERIOR = 116
+THIRD_FLOOR = 117
+LATERAL_FLOOR = 118
+ZERO_SOLID_TRACTION = 1000
+CHOROID_PLEXUS_LATERAL_ZERO_TRACTION = 1001
+
+# Cell tags
+CANAL = 3
+THIRD_VENTRICLE = 4
+AQUEDUCT = 5
+FORAMINA_34 = 6
+LATERAL_VENTRICLES = 7
+FOURTH_VENTRICLE = 8
 
 write_output = int(sys.argv[1])
 comm = MPI.COMM_WORLD
@@ -53,9 +79,10 @@ with dfx.io.XDMFFile(comm, f"../geometries/ventricles_{mesh_suffix}.xdmf", "r") 
 
 ds = ufl.Measure('ds', domain=mesh, subdomain_data=ft) # Boundary integral measure
 dx = ufl.Measure('dx', domain=mesh, subdomain_data=ct) # Volume integral measure
+n = ufl.FacetNormal(mesh)
 
 # Material parameters
-E = float(sys.argv[3]) #3156 # Modulus of elasticity [Pa]
+E = float(sys.argv[3]) # Modulus of elasticity [Pa]
 nu = 0.479 # Poisson's ratio [-]
 eta_value = 2*E / (1+nu) # First Lamé parameter value
 lam_value = nu*E / ((1+nu) * (1-2*nu)) # Second Lamé parameter value
@@ -82,8 +109,10 @@ write_time = 0
 
 # Definte finite element and function space
 p = int(sys.argv[2])
-vec_el = element("Lagrange", mesh.basix_cell(), p, shape=(mesh.geometry.dim,))
+vec_el  = element("Lagrange", mesh.basix_cell(), p, shape=(mesh.geometry.dim,))
+scal_el = element("Lagrange", mesh.basix_cell(), p)
 W = dfx.fem.functionspace(mesh, vec_el)
+V = dfx.fem.functionspace(mesh, scal_el)
 
 # Extract subspaces
 W_x = W.sub(0) # x displacement space
@@ -115,8 +144,8 @@ eps   = lambda arg: sym(grad(arg)) # The symmetric gradient
 sigma = lambda w: 2.0*eta*eps(w) + lam*ufl.tr(eps(w))*ufl.Identity(mesh.geometry.dim) # Stress tensor
 
 # Damping parameters
-eta_M = dfx.fem.Constant(mesh, 0.50) # Damping proportional to inertia
-eta_K = dfx.fem.Constant(mesh, 0.25) # Damping proportional to stiffness 
+eta_M = dfx.fem.Constant(mesh, 0.20) # Damping proportional to inertia
+eta_K = dfx.fem.Constant(mesh, 0.10) # Damping proportional to stiffness 
 
 # The weak form
 a = rho/(beta*dt**2)*inner(w, dw)*dx + inner(sigma(w), eps(dw)) * dx
@@ -126,6 +155,36 @@ L = rho*inner(wh_n/(beta*dt**2) + wh_dot_n/(beta*dt) + (1-2*beta)/(2*beta)*wh_dd
 a += gamma/(beta*dt) * (eta_M*rho*inner(w, dw)*dx + eta_K*inner(sigma(w), eps(dw))*dx)
 v_res = wh_dot_n + dt*wh_ddot_n*(1-gamma) - gamma/(beta*dt)*(wh_n + dt*wh_dot_n) + dt*gamma*wh_ddot_n*(2*beta-1)/(2*beta)
 L -= (eta_M*rho* inner(v_res, dw)*dx + eta_K * inner(sigma(v_res), eps(dw))*dx)
+
+
+# Impose normal displacement weakly with Nitsche
+# h = dfx.cpp.mesh.h(mesh._cpp_object, tdim, np.arange(mesh.topology.index_map(tdim).size_local, dtype=np.int32)).max()
+h = ufl.CellDiameter(mesh)
+ds = ufl.Measure("ds", domain=mesh, subdomain_data=ft)
+t = lambda w: dot(sigma(w), n)
+w_normal_LV_lat = dfx.fem.Constant(mesh, 0.0)
+w_normal_LV_cec = dfx.fem.Function(V)
+w_normal_LV_horns = dfx.fem.Constant(mesh, 0.0)
+w_normal_3V = dfx.fem.Constant(mesh, 0.0)
+phi = dfx.fem.Constant(mesh, 20000000.0) #*(mesh_suffix+1)
+
+lateral_tags_cec = (CORPUS_CALLOSUM)
+lateral_tags_horns = (LATERAL_VENTRICLES_WALL)
+lateral_tags_lat = (LATERAL_LEFT, LATERAL_RIGHT)
+third_tags = (THIRD_LEFT, THIRD_RIGHT, THIRD_ANTERIOR, THIRD_POSTERIOR, CHOROID_PLEXUS_THIRD, THIRD_VENTRICLE_WALL)
+a += -inner(dot(t(w), n), dot(dw, n))*(ds(lateral_tags_lat) + ds(lateral_tags_cec) + ds(lateral_tags_horns) + ds(third_tags))
+
+a += -inner(dot(w, n), dot(t(dw),n))*(ds(lateral_tags_lat) + ds(lateral_tags_cec) + ds(lateral_tags_horns) + ds(third_tags)) \
+    + phi/h*inner(dot(w, n), dot(dw, n))*(ds(lateral_tags_lat) + ds(lateral_tags_cec) + ds(lateral_tags_horns) + ds(third_tags))
+
+L += -inner(w_normal_LV_lat, dot(t(dw), n))*ds(lateral_tags_lat) \
+    + phi/h*inner(w_normal_LV_lat, dot(dw, n))*ds(lateral_tags_lat) \
+    - inner(w_normal_LV_cec, dot(t(dw), n))*ds(lateral_tags_cec) \
+    + phi/h*inner(w_normal_LV_cec, dot(dw, n))*ds(lateral_tags_cec) \
+    - inner(w_normal_LV_horns, dot(t(dw), n))*ds(lateral_tags_horns) \
+    + phi/h*inner(w_normal_LV_horns, dot(dw, n))*ds(lateral_tags_horns) \
+    - inner(w_normal_3V, dot(t(dw), n))*ds(third_tags) \
+    + phi/h*inner(w_normal_3V, dot(dw, n))*ds(third_tags)
     
 # Create linear system
 a_cpp, L_cpp = dfx.fem.form(a), dfx.fem.form(L)
@@ -141,79 +200,74 @@ b = create_vector(L_cpp)
 
 # Get displacement expressions
 cc_disp_expr = DisplacementCorpusCallosumCephalocaudal(period=period, timestep=timestep, final_time=T)
+cc_disp_expr.y_max = comm.allreduce(mesh.geometry.x[:, 1].max(), op=MPI.MAX)
 tv_disp_expr = DisplacementThirdVentricleLateral(period=period, timestep=timestep, final_time=T)
 lv_disp_expr = DisplacementCaudateNucleusHeadLateral(period=period, timestep=timestep, final_time=T)
+horns_expr   = DisplacementLateralVentricleHorns(period=period, timestep=timestep, final_time=T)
 
-# Create finite element functions for the BCs
-cc_disp_func = dfx.fem.Function(W)
-tv_disp_func_right = dfx.fem.Function(W)
-tv_disp_func_left  = dfx.fem.Function(W)
-lv_disp_func_right = dfx.fem.Function(W)
-lv_disp_func_left  = dfx.fem.Function(W)
 bcs = []
 
-cc_dofs = dfx.fem.locate_dofs_topological((W_z, W), facet_dim, ft.find(CORPUS_CALLOSUM))
-bcs.append(dfx.fem.dirichletbc(cc_disp_func, cc_dofs, W_z))
+w_LV_floor = dfx.fem.Function(W)
+w_3V_floor = dfx.fem.Function(W)
+floor_expr = DisplacementVentricleFloorCephalocaudal(period=period, timestep=timestep, final_time=T)
+dofs_LV_floor = dfx.fem.locate_dofs_topological((W_z, W), facet_dim, np.concatenate((ft.find(LATERAL_FLOOR), ft.find(CHOROID_PLEXUS_LATERAL))))
+dofs_3V_floor = dfx.fem.locate_dofs_topological((W_z, W), facet_dim, ft.find(THIRD_FLOOR))
+bcs.append(dfx.fem.dirichletbc(w_LV_floor, dofs_LV_floor, W_z))
+bcs.append(dfx.fem.dirichletbc(w_3V_floor, dofs_3V_floor, W_z))
 
-lv_dofs_right = dfx.fem.locate_dofs_topological((W_x, W), facet_dim, ft.find(LATERAL_RIGHT))
-bcs.append(dfx.fem.dirichletbc(lv_disp_func_right, lv_dofs_right, W_x))
-
-lv_dofs_left  = dfx.fem.locate_dofs_topological((W_x, W), facet_dim, ft.find(LATERAL_LEFT))
-bcs.append(dfx.fem.dirichletbc(lv_disp_func_left, lv_dofs_left, W_x))
-
-tv_dofs_right = dfx.fem.locate_dofs_topological((W_x, W), facet_dim, ft.find(THIRD_RIGHT))
-bcs.append(dfx.fem.dirichletbc(tv_disp_func_right, tv_dofs_right, W_x))
-
-tv_dofs_left  = dfx.fem.locate_dofs_topological((W_x, W), facet_dim, ft.find(THIRD_LEFT))
-bcs.append(dfx.fem.dirichletbc(tv_disp_func_left, tv_dofs_left, W_x))
-
-# Anchoring the perimeter of the canal outlet requires
-# finding the vertices on this perimeter, which
-# are the vertices that neighbor facets of
-# the two different boundaries (canal wall and canal outlet)
-
+# Find vertices on the perimeter of the outlet
+# Start by creating required connectivities 
+edge_dim = facet_dim-1
+vertex_dim = 0
+mesh.topology.create_connectivity(facet_dim, edge_dim)  # Facets (dim-1) to edges (dim-2)
+mesh.topology.create_connectivity(edge_dim, vertex_dim) # Edges (dim-2) to vertices (dim=0)
+f_to_e = mesh.topology.connectivity(facet_dim, edge_dim)
+e_to_v = mesh.topology.connectivity(edge_dim, vertex_dim)
 # First get the facets of the outlet and the canal wall
 outlet_facets = ft.indices[ft.values==CANAL_OUT]
 wall_facets = ft.indices[ft.values==CANAL_WALL]
 
-# Get the connectivity from facets (dim-1) to vertices (dim=0)
-mesh.topology.create_connectivity(facet_dim, 0)
-f_to_v = mesh.topology.connectivity(facet_dim, 0)
-
-# Find the outlet and wall vertices in a manner that
+# Find the outlet and wall edges in a manner that
 # is consistent for parallel communication
 if len(outlet_facets) > 0:
-    local_outlet_vertices = np.unique(np.concatenate([f_to_v.links(f) for f in outlet_facets]))
+    local_outlet_edges = np.unique(np.concatenate([f_to_e.links(f) for f in outlet_facets]))
 else:
-    local_outlet_vertices = np.array([], dtype=np.int32)
+    local_outlet_edges = np.array([], dtype=np.int32)
 
 if len(wall_facets) > 0:
-    local_wall_vertices = np.unique(np.concatenate([f_to_v.links(f) for f in wall_facets]))
+    local_wall_edges = np.unique(np.concatenate([f_to_e.links(f) for f in wall_facets]))
 else:
-    local_wall_vertices = np.array([], dtype=np.int32)
+    local_wall_edges = np.array([], dtype=np.int32)
 
-# The perimeter vertices are the intersection of the two sets of vertices.
-# Gather all local vertex arrays from all processes and
-# create a single global array of unique vertices.
+# The perimeter edges are the intersection of the two sets of edges.
+# Gather all local edge arrays from all processes and
+# create a single global array of unique edges.
 # Then use the global arrays to compute intersection globally
-all_outlet_vertices_list = comm.allgather(local_outlet_vertices) 
-global_outlet_vertices = np.unique(np.concatenate(all_outlet_vertices_list)) 
-all_wall_vertices_list = comm.allgather(local_wall_vertices)
-global_wall_vertices = np.unique(np.concatenate(all_wall_vertices_list))
-outlet_perimeter_vertices = np.intersect1d(global_outlet_vertices, global_wall_vertices)
+all_outlet_edges = comm.allgather(local_outlet_edges) 
+global_outlet_edges = np.unique(np.concatenate(all_outlet_edges)) 
+all_wall_edges = comm.allgather(local_wall_edges)
+global_wall_edges = np.unique(np.concatenate(all_wall_edges))
+outlet_perimeter_edges = np.intersect1d(global_outlet_edges, global_wall_edges)
+
+# Find the vertices that lie on the perimeter edges
+if len(outlet_perimeter_edges)>0:
+    outlet_perimeter_vertices = np.unique(np.concatenate([e_to_v.links(e) for e in outlet_perimeter_edges]))
+else:
+    outlet_perimeter_vertices = np.array([], dtype=np.int32)
 
 # Find which outlet_perimeter_vertices are owned locally on the process
 owned_vertex_range = mesh.topology.index_map(0).local_range # Get the start and end+1 of the owned vertex range on this process
 
 # Create a boolean mask to filter the vertices
 is_owned = (outlet_perimeter_vertices >= owned_vertex_range[0]) & \
-           (outlet_perimeter_vertices < owned_vertex_range[1])
+           (outlet_perimeter_vertices  < owned_vertex_range[1])
 
 # Apply the mask to get only the vertices owned by this process
 local_perimeter_vertices = outlet_perimeter_vertices[is_owned]
 
 # Now find the outlet perimeter dofs that are located at the vertices found
 mesh.topology.create_connectivity(0, mesh.topology.dim) # Create connectivity from vertices (dim=0) to cells (dim)
+
 outlet_perimeter_dofs_x = dfx.fem.locate_dofs_topological((W_x, W), 0, local_perimeter_vertices)
 outlet_perimeter_dofs_y = dfx.fem.locate_dofs_topological((W_y, W), 0, local_perimeter_vertices)
 
@@ -224,6 +278,11 @@ bcs.append(dfx.fem.dirichletbc(zero, outlet_perimeter_dofs_y, W_y))
 # Assemble the system matrix and the RHS vector
 A, b = assemble_system(A, b, a_cpp, L_cpp, bcs)
 
+
+# Create the solver object
+solver = PETSc.KSP().create(comm)
+solver.setOperators(A)
+
 # Configure direct linear solver using MUMPS.
 # The system matrix is singular due to rigid body motions
 # not being sufficiently constrained by the BCs, so
@@ -232,13 +291,10 @@ opts = PETSc.Options()
 opts["ksp_type"] = "preonly"
 opts["pc_type"] = "lu"
 opts["pc_factor_mat_solver_type"] = "mumps"
+opts["mat_mumps_icntl_14"] = 80  # Increase MUMPS working memory
 opts["mat_mumps_icntl_24"] = 1  # Option to support solving a singular matrix (rigid motions nullspace)
 opts["mat_mumps_icntl_25"] = 0  # Option to support solving a singular matrix (rigid motions nullspace)
 opts["ksp_error_if_not_converged"] = 1 # Throw an error if KSP solver does not converge
-
-# Create the solver object, set options and enable convergence monitoring
-solver = PETSc.KSP().create(comm)
-solver.setOperators(A)
 solver.setFromOptions()
 
 # Prepare output functions and files
@@ -249,13 +305,13 @@ CG1_vector_space = dfx.fem.functionspace(mesh,
                                                          shape=(mesh.geometry.dim,)))
 wh_out = dfx.fem.Function(CG1_vector_space)
 vh_out = dfx.fem.Function(CG1_vector_space)
-k = 1 # BDM element degree
+k = int(sys.argv[6]) # BDM element degree
 bdm_el = element("BDM", mesh.basix_cell(), k)
 BDM = dfx.fem.functionspace(mesh, bdm_el)
 dw_dt_bdm = dfx.fem.Function(BDM)
 dw_dt_bdm.name = "defo_velocity"
 wh.name = "defo_displacement"
-output_dir = f"../output/mesh_{mesh_suffix}/deformation_p={p}_E={E:.0f}_T={T:.0f}/"
+output_dir = f"../output/mesh_{mesh_suffix}/deformation_p={p}_E={E:.0f}_k={k}_T={T:.0f}/"
 data_dir = output_dir+"data/"
 Path(data_dir).mkdir(parents=True, exist_ok=True)
 
@@ -274,7 +330,6 @@ if write_output:
 # displacement velocity into a Brezzi-Douglas-Marini
 # finite element space
 projection_problem = projection_problem_CG_to_BDM(wh_dot, dw_dt_bdm, dx)
-
 # Define some quantities that will be calculated during
 # the solution loop.
 # Energy norms
@@ -302,6 +357,8 @@ if len(colliding_cells.links(0))>0:
 applied_bc1 = []
 applied_bc2 = []
 applied_bc3 = []
+applied_bc4 = []
+applied_bc5 = []
 
 for i, t in enumerate(times[1:], 1):
     
@@ -309,18 +366,24 @@ for i, t in enumerate(times[1:], 1):
 
     # Update displacement BCs
     cc_disp_expr.increment_index(t)
-    cc_disp_expr()
-    cc_disp_func.x.array[:] = cc_disp_expr.amplitude
-
-    tv_disp_expr.increment_index(t)
-    tv_disp_expr()
-    tv_disp_func_right.x.array[:] = tv_disp_expr.amplitude
-    tv_disp_func_left.x.array[:] = -1.0*tv_disp_expr.amplitude
+    w_normal_LV_cec.interpolate(cc_disp_expr)
 
     lv_disp_expr.increment_index(t)
     lv_disp_expr()
-    lv_disp_func_right.x.array[:] = lv_disp_expr.amplitude
-    lv_disp_func_left.x.array[:] = -1.0*lv_disp_expr.amplitude
+    w_normal_LV_lat.value = lv_disp_expr.amplitude
+
+    tv_disp_expr.increment_index(t)
+    tv_disp_expr()
+    w_normal_3V.value = tv_disp_expr.amplitude
+
+    floor_expr.increment_index(t)
+    floor_expr()
+    w_LV_floor.x.array[:] = .5*floor_expr.amplitude
+    w_3V_floor.x.array[:] = .5*floor_expr.amplitude
+
+    horns_expr.increment_index(t)
+    horns_expr()
+    w_normal_LV_horns.value = horns_expr.amplitude
     
     # Assemble right-hand side
     with b.localForm() as local_b_vec: local_b_vec.set(0.0)
@@ -354,11 +417,14 @@ for i, t in enumerate(times[1:], 1):
                                 + wh.sub(1).collapse().x.array**2
                                 + wh.sub(2).collapse().x.array**2).max(),
                                 op=MPI.MAX)
+    print(f"Maximum displacement magnitude: {max_disp[i, 3]:.1e}")
     
     # Store the applied BCs (equal on all processes)
     applied_bc1.append(cc_disp_expr.amplitude)
     applied_bc2.append(tv_disp_expr.amplitude)
     applied_bc3.append(lv_disp_expr.amplitude)
+    applied_bc4.append(floor_expr.amplitude)
+    applied_bc5.append(horns_expr.amplitude)
 
     if t >= final_period_start and write_output:
         # Project velocity and write output
@@ -399,18 +465,24 @@ if comm.rank==0:
     ax.plot(times[1:], np.array(applied_bc1), 'r', label="Corpus callosum")
     ax.plot(times[1:], np.array(applied_bc2), 'b', label="3V wall")
     ax.plot(times[1:], np.array(applied_bc3), 'g', label="Caudate nucleus")
+    ax.plot(times[1:], np.array(applied_bc4), 'm', label="LV/3V floor")
+    ax.plot(times[1:], np.array(applied_bc5), 'k', label="Inf./occ. horns")
     ax.legend()
-    fig.savefig(fig_dir+f"applied_BCs_deformation_p={p}_E={E:.0f}_dt={timestep}.png")
+    fig.savefig(fig_dir+f"applied_BCs_deformation_p={p}_E={E:.0f}_k={k}_T={T}_dt={timestep}.png")
     plt.show()
     np.save(data_dir+f"applied_bc_corpus_callosum_dt={timestep}.npy", np.array(applied_bc1))
     np.save(data_dir+f"applied_bc_3V_wall_dt={timestep}.npy", np.array(applied_bc2))
     np.save(data_dir+f"applied_bc_caudate_nucleus_dt={timestep}.npy", np.array(applied_bc3))
+    np.save(data_dir+f"applied_bc_LV_3V_floor_dt={timestep}.npy", np.array(applied_bc4))
+    np.save(data_dir+f"applied_bc_inf_occ_horns_dt={timestep}.npy", np.array(applied_bc5))
 
     fig2, ax2 = plt.subplots(figsize=([12, 8]))
+    ax2_ = ax2.twinx()
     ax2.plot(times, energy[:, 0], 'r', label="Kinetic energy")
-    ax2.plot(times, energy[:, 1], 'b', label="Elastic energy")
+    ax2_.plot(times, energy[:, 1], color='b', label="Elastic energy", marker='^', markevery=int(len(times)/25))
     ax2.legend()
-    fig2.savefig(fig_dir+f"MPI={comm.size}_energies_p={p}_E={E:.0f}_dt={timestep}.png")
+    ax2_.legend()
+    fig2.savefig(fig_dir+f"MPI={comm.size}_energies_p={p}_E={E:.0f}_k={k}_T={T}_dt={timestep}.png")
     plt.show()
     np.save(data_dir+f"energies_dt={timestep}.npy", energy)
 
@@ -423,7 +495,7 @@ if comm.rank==0:
     ax3.plot(times, point_disp[:, 1], 'c', label="Point y disp")
     ax3.plot(times, point_disp[:, 2], 'm', label="Point z disp")
     ax3.legend()
-    fig3.savefig(fig_dir+f"MPI={comm.size}_displacements_p={p}_E={E:.0f}_dt={timestep}.png")
+    fig3.savefig(fig_dir+f"MPI={comm.size}_displacements_p={p}_E={E:.0f}_k={k}_T={T}_dt={timestep}.png")
     plt.show()
     np.save(data_dir+f"max_displacements_dt={timestep}.npy", max_disp)
     np.save(data_dir+f"point_displacements_dt={timestep}.npy", point_disp)
--- src/utilities/deformation_data.py
+++ src/utilities/deformation_data.py
@@ -6,6 +6,8 @@ class DisplacementCorpusCallosumCephalocaudal:
     """ Displacement curve for the corpus callosum in the cephalocaudal (feet-head) direction
         (z axis, positive in cranial direction (towards head)).
         Adapted from Kurtcuoglu et al. (2007), Soellinger et al. (2007, 2009)."""
+
+    y_max = None
     
     def __init__(self, period: float, timestep: float, final_time: float):
         """ Constructor. Assumes period and final_time are integers. """
@@ -20,7 +22,7 @@ class DisplacementCorpusCallosumCephalocaudal:
     def set_data(self):
         x = np.linspace(0.0, 1.0, 11)
         y = [0.0, 0.015, 0.011, -0.050, -0.075, -0.0625, -0.0475, -0.05, -0.0475, -0.03, 0.0]
-        disp_data = np.array(y)*1e-3 # Convert from mm to m
+        disp_data = 2/5*np.array(y)*1e-3 # Convert from mm to m
         times = np.array(x)
         disp_interp = interp.CubicSpline(times, disp_data, bc_type="periodic")
         refined_times = np.linspace(times[0], times[-1], self.N)
@@ -45,11 +47,15 @@ class DisplacementCorpusCallosumCephalocaudal:
             return 1.0
         return (6/(2*self.P)**5)*self.t**5 - (15/(2*self.P)**4)*self.t**4 + (10/(2*self.P)**3)*self.t**3
 
-    def __call__(self):
+    def __call__(self, x=None):
         """ Evaluates displacement spline function.
         """
         scale_factor = self.scaling()
         self.amplitude = scale_factor*self.disp_final[self.index]
+        if x is not None:
+            linear_factor = 1/2*(1 + (self.y_max - x[1])/(2*self.y_max))
+
+            return linear_factor * self.amplitude
 
 class DisplacementCaudateNucleusHeadLateral:
     """ Displacement curve for the head of the caudate nucleus
@@ -74,7 +80,7 @@ class DisplacementCaudateNucleusHeadLateral:
     def set_data(self):
         x = np.linspace(0.0, 1.0, 11)
         y = [0.0, -0.01, -0.03, -0.035, -0.025, -0.02, -0.018, -0.016, -0.009, -0.003, 0.0]
-        disp_data = np.array(y)*1e-3 # Convert from mm to m
+        disp_data = 2/5*1/2*np.array(y)*1e-3 # Convert from mm to m
         times = np.array(x)
         disp_interp = interp.CubicSpline(times, disp_data, bc_type="periodic")
         refined_times = np.linspace(times[0], times[-1], self.N)
@@ -127,7 +133,102 @@ class DisplacementThirdVentricleLateral:
     def set_data(self):
         x = np.linspace(0.0, 1.0, 11)
         y = [0.0, 0.005, -0.02, -0.03, -0.0285, -0.0275, -0.0225, -0.015, -0.0075, -0.005, 0.0]
-        disp_data = np.array(y)*1e-3 # Convert from mm to m
+        disp_data = 2/5*1/2*np.array(y)*1e-3 # Convert from mm to m
+        times = np.array(x)
+        disp_interp = interp.CubicSpline(times, disp_data, bc_type="periodic")
+        refined_times = np.linspace(times[0], times[-1], self.N)
+        num_final_periods = int(self.T) - int(self.P)
+        
+        # Construct interpolated deformation data that is windowed 
+        # in the initial period
+        window = tukey(len(refined_times), alpha=0.25)
+        halfway_index = int(self.N/2)
+        disp_first_half = window[:halfway_index]*disp_interp(refined_times, 0)[:halfway_index]
+        disp_second_half = disp_interp(refined_times, 0)[halfway_index:]
+        first_period_windowed = np.concatenate((disp_first_half, disp_second_half))
+        final_periods = np.concatenate(([disp_interp(refined_times, 0)[1:] for _ in range(num_final_periods)]))
+        self.disp_final = np.concatenate((first_period_windowed, final_periods))
+    
+    def increment_index(self, t: float):
+        self.t = t
+        self.index += 1
+
+    def scaling(self):
+        if self.t >= (2*self.P):
+            return 1.0
+        return (6/(2*self.P)**5)*self.t**5 - (15/(2*self.P)**4)*self.t**4 + (10/(2*self.P)**3)*self.t**3
+
+    def __call__(self):
+        """ Evaluates displacement spline function.
+        """
+        scale_factor = self.scaling()
+        self.amplitude = scale_factor*self.disp_final[self.index]
+
+class DisplacementLateralVentricleHorns:
+    """ UPDATE """
+
+    def __init__(self, period: float, timestep: float, final_time: float):
+        """ Constructor. Assumes period and final_time are integers. """
+        self.t = 0.0
+        self.P = period
+        self.N = int(period / timestep)+1
+        self.T = final_time
+        assert final_time > 2*period, print("Ramp up occurs over two periods, ensure final time > 2*period.")
+        self.index = 0
+        self.set_data()
+    
+    def set_data(self):
+        x = np.linspace(0.0, 1.0, 11)
+        y = [0.0, -0.01, -0.020, -0.050, -0.075, -0.0625, -0.0475, -0.05, -0.0475, -0.03, 0.0]
+        disp_data = 2/5*1/4*np.array(y)*1e-3 # Convert from mm to m
+        times = np.array(x)
+        disp_interp = interp.CubicSpline(times, disp_data, bc_type="periodic")
+        refined_times = np.linspace(times[0], times[-1], self.N)
+        num_final_periods = int(self.T) - int(self.P)
+        
+        # Construct interpolated deformation data that is windowed 
+        # in the initial period
+        window = tukey(len(refined_times), alpha=0.25)
+        halfway_index = int(self.N/2)
+        disp_first_half = window[:halfway_index]*disp_interp(refined_times, 0)[:halfway_index]
+        disp_second_half = disp_interp(refined_times, 0)[halfway_index:]
+        first_period_windowed = np.concatenate((disp_first_half, disp_second_half))
+        final_periods = np.concatenate(([disp_interp(refined_times, 0)[1:] for _ in range(num_final_periods)]))
+        self.disp_final = np.concatenate((first_period_windowed, final_periods))
+    
+    def increment_index(self, t: float):
+        self.t = t
+        self.index += 1
+
+    def scaling(self):
+        if self.t >= (2*self.P):
+            return 1.0
+        return (6/(2*self.P)**5)*self.t**5 - (15/(2*self.P)**4)*self.t**4 + (10/(2*self.P)**3)*self.t**3
+
+    def __call__(self):
+        """ Evaluates displacement spline function.
+        """
+        scale_factor = self.scaling()
+        self.amplitude = scale_factor*self.disp_final[self.index]
+
+
+class DisplacementVentricleFloorCephalocaudal:
+    """ UPDATE """
+
+    def __init__(self, period: float, timestep: float, final_time: float):
+        """ Constructor. Assumes period and final_time are integers. """
+        self.t = 0.0
+        self.P = period
+        self.N = int(period / timestep)+1
+        self.T = final_time
+        assert final_time > 2*period, print("Ramp up occurs over two periods, ensure final time > 2*period.")
+        self.index = 0
+        self.set_data()
+    
+    def set_data(self):
+        x = np.linspace(0.0, 1.0, 11)
+        y = [0.0, 0.015, 0.011, -0.050, -0.075, -0.0625, -0.0525, -0.05, -0.0475, -0.03, 0.0]
+        disp_data = 2/5*1/2*np.array(y)*1e-3 # Convert from mm to m
         times = np.array(x)
         disp_interp = interp.CubicSpline(times, disp_data, bc_type="periodic")
         refined_times = np.linspace(times[0], times[-1], self.N)
