65b0bf8167eb718bbcf437fb17dad05c61937d4c
hherlyng <hherlyng@hotmail.com>
2025-10-14 10:35:03 +0200

    FTLE figures human 1 and 2

--- src/plot_colorbar.py
+++ src/plot_colorbar.py
@@ -24,18 +24,20 @@ if __name__=='__main__':
             cmap = getattr(colormaps, cmap_name)
         except:
             cmap = matplotlib.colormaps.get_cmap(cmap_name)
-
+        
+        cmap = cmap.resampled(16)
         cb = matplotlib.colorbar.ColorbarBase(ax, cmap=cmap, orientation='horizontal')
         cb_max = 0.1
         cb.set_ticks([0, 0.25, 0.5, 0.75, 1.0])
-        cb.set_ticklabels([0,
-                           f"{0.25*cb_max:.2g}",
-                           f"{0.50*cb_max:.2g}",
-                           f"{0.75*cb_max:.2g}",
-                           f"{cb_max:.2g}"])
+        # cb.set_ticklabels([0,
+        #                    f"{0.25*cb_max:.2g}",
+        #                    f"{0.50*cb_max:.2g}",
+        #                    f"{0.75*cb_max:.2g}",
+        #                    f"{cb_max:.2g}"])
+        cb.set_ticklabels(["Low", " ", " ", " ", "High"])
         ax.tick_params(axis='x', labelsize=35)
-        ax.set_title(r"Attracting FTLE value", fontsize=46, pad=20.0)
-        fig_name = f"../output/illustrations/colorbars/{orientation}_colorbar_{cmap_name}.png"
+        ax.set_title(r"Repelling FTLE value", fontsize=46, pad=20.0)
+        fig_name = f"../output/illustrations/colorbars/{orientation}_colorbar_{cmap_name}_low_high.png"
 
     fig.tight_layout(pad=0)
     fig.savefig(fig_name, bbox_inches=[])
