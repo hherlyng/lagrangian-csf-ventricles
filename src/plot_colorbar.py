de511f2be23df3bf14acac8ac38f41be1be6459e
hherlyng <hherlyng@hotmail.com>
2025-10-21 11:11:45 +0200

    add zfish flow fig, write FTLE results

--- src/plot_colorbar.py
+++ src/plot_colorbar.py
@@ -16,6 +16,7 @@ if __name__=='__main__':
     
     orientation = sys.argv[1]
     cmap_name = sys.argv[2]
+    n_colors = int(sys.argv[3])
 
     if orientation=="horizontal":
         fig = plt.figure(figsize=(8, 1.75))
@@ -25,19 +26,20 @@ if __name__=='__main__':
         except:
             cmap = matplotlib.colormaps.get_cmap(cmap_name)
         
-        cmap = cmap.resampled(16)
+        cmap = cmap.resampled(n_colors)
         cb = matplotlib.colorbar.ColorbarBase(ax, cmap=cmap, orientation='horizontal')
-        cb_max = 0.1
-        cb.set_ticks([0, 0.25, 0.5, 0.75, 1.0])
-        # cb.set_ticklabels([0,
-        #                    f"{0.25*cb_max:.2g}",
-        #                    f"{0.50*cb_max:.2g}",
-        #                    f"{0.75*cb_max:.2g}",
-        #                    f"{cb_max:.2g}"])
-        cb.set_ticklabels(["Low", " ", " ", " ", "High"])
+        cb_max = 26.4
+        # cb.set_ticks([0, 0.25, 0.5, 0.75, 1.0])
+        cb.set_ticks([0, 1.0])
+        cb.set_ticklabels([0,
+                        #    f"{0.25*cb_max:.2g}",
+                        #    f"{0.50*cb_max:.2g}",
+                        #    f"{0.75*cb_max:.2g}",
+                           f"{cb_max:.3g}"])
+        # cb.set_ticklabels(["Low", " ", " ", " ", "High"])
         ax.tick_params(axis='x', labelsize=35)
-        ax.set_title(r"Attracting FTLE value", fontsize=46, pad=20.0)
-        fig_name = f"../output/illustrations/colorbars/{orientation}_colorbar_{cmap_name}_low_high.png"
+        ax.set_title(r"Velocity [$\mu$m/s]", fontsize=46, pad=20.0)
+        fig_name = f"../output/illustrations/colorbars/{orientation}_colorbar_{cmap_name}_zfish.png"
 
     fig.tight_layout(pad=0)
     fig.savefig(fig_name, bbox_inches=[])
