07def3f32cf69c82ac213bb6957bc20e865ffe79
hherlyng <hherlyng@hotmail.com>
2025-12-16 17:21:14 +0100

    update colorbars

--- src/plot_colorbar.py
+++ src/plot_colorbar.py
@@ -5,10 +5,10 @@ import colormaps
 
 # Set matplotlib properties
 plt.rcParams.update({
-    "font.family" : "Serif",
+    "font.family" : "serif",
+    "font.serif" : ["Times New Roman"],
     "text.usetex" : True,
-    "axes.spines.top" : False,
-    "axes.spines.right" : False
+    "text.latex.preamble": r"\usepackage{mathptmx}"
 })
 
 if __name__=='__main__':
@@ -28,18 +28,19 @@ if __name__=='__main__':
         
         cmap = cmap.resampled(n_colors)
         cb = matplotlib.colorbar.ColorbarBase(ax, cmap=cmap, orientation='horizontal')
-        cb_max = 26.4
+        cb_max = 0.1 # 26.4
         # cb.set_ticks([0, 0.25, 0.5, 0.75, 1.0])
         cb.set_ticks([0, 1.0])
-        cb.set_ticklabels([0,
-                        #    f"{0.25*cb_max:.2g}",
-                        #    f"{0.50*cb_max:.2g}",
-                        #    f"{0.75*cb_max:.2g}",
-                           f"{cb_max:.3g}"])
-        # cb.set_ticklabels(["Low", " ", " ", " ", "High"])
+        # cb.set_ticklabels([0,
+        #                    f"{0.25*cb_max:.2g}",
+        #                    f"{0.50*cb_max:.2g}",
+        #                    f"{0.75*cb_max:.2g}",
+        #                    f"{cb_max:.3g}"])
+        cb.set_ticklabels(["Low", "High"])
         ax.tick_params(axis='x', labelsize=35)
-        ax.set_title(r"Velocity [$\mu$m/s]", fontsize=46, pad=20.0)
-        fig_name = f"../output/illustrations/colorbars/{orientation}_colorbar_{cmap_name}_zfish.png"
+        ax.set_title(r"Backward FTLE value", fontsize=46, pad=20.0)
+        # ax.set_title(r"Velocity [$\mu$m/s]", fontsize=46, pad=20.0)
+        fig_name = f"../output/illustrations/colorbars/{orientation}_colorbar_{cmap_name}_low_high.png"
 
     fig.tight_layout(pad=0)
     fig.savefig(fig_name, bbox_inches=[])
